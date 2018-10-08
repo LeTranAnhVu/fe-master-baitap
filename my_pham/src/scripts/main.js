3c2caced355fe1c80255fb8a7363b65279138227
LeTranAnhVu <vule5726@gmail.com>
2018-10-09 00:40:22 +0700

    my_pham done 90%

--- my_pham/src/scripts/main.js
+++ my_pham/src/scripts/main.js
@@ -1,2 +1,24 @@
 // Main
 console.log('FullStack Vietnam!')
+
+// carousel
+$(document).ready(function(){
+	$('.main-carousel').owlCarousel({
+		items: 1,
+		dots: false,
+		nav: true
+	});
+
+	$('.minor-carousel').owlCarousel({
+		items: 4,
+		dots: false,
+		nav: true
+	});
+
+	$('.best-seller-carousel').owlCarousel({
+		items: 1,
+		dots: false,
+		nav: true
+	});
+  });
+  
