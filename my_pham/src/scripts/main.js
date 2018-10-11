2c86fc9add2f24e5fc1bf67c563c8c77742fb44d
LeTranAnhVu <vule5726@gmail.com>
2018-10-11 22:44:51 +0700

    my pham done

--- my_pham/src/scripts/main.js
+++ my_pham/src/scripts/main.js
@@ -6,19 +6,33 @@ $(document).ready(function(){
 	$('.main-carousel').owlCarousel({
 		items: 1,
 		dots: false,
-		nav: true
+		nav: true,
+		loop: true,
+		autoplay: true,
+		autoplayTimeout: 5000,
+		autoplayHoverPause: true,
 	});
 
 	$('.minor-carousel').owlCarousel({
+		loop: true,
 		items: 4,
 		dots: false,
-		nav: true
+		nav: true,
+		autoplay: true,
+		autoplayTimeout: 3000,
+    	autoplayHoverPause: true,
+		
 	});
 
 	$('.best-seller-carousel').owlCarousel({
 		items: 1,
 		dots: false,
 		nav: true,
+		loop: true,
+		autoplay: true,
+		autoplayTimeout: 3000,
+		autoplayHoverPause: true,
+		navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"]
 	});
   });
   
