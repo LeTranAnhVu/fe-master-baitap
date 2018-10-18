453399856301c42ed96773dc84614cc700cbf110
LeTranAnhVu <vule5726@gmail.com>
2018-10-19 00:58:49 +0700

    fullstack_vietnam done 80%

--- Fullstack_Vietnam/src/scripts/main.js
+++ Fullstack_Vietnam/src/scripts/main.js
@@ -1,2 +1,38 @@
 // Main
 console.log('FullStack Vietnam!')
+
+
+$(document).ready(function(){
+	$('.main-carousel').owlCarousel({
+		items: 1,
+		loop: true,
+		autoplay: true,
+		autoplayTimeout: 5000,
+	});
+	$('.customers-carousel').owlCarousel({
+		loop: true,
+		responsiveClass: true,
+		responsive:{
+			0:{
+				items: 5,
+				nav: false,
+				dots: true
+			},
+			540:{
+				items: 5,
+				nav: false,
+				dots: true
+			},
+			720:{
+				items: 5,
+				nav: true,
+			},
+			1000:{
+				items: 6,
+				nav: true,
+				dots: false,
+				navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"]
+			}
+		}
+	});
+  });
